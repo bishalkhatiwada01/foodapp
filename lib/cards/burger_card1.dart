851f54f033ffe325a436d6ba0678c458bdcbcba0
bishalkhatiwada <bishal.khatiwada.sibs.edu@gmail.com>
2023-04-07 12:15:56 +0545

    today

--- lib/cards/burger_card1.dart
+++ lib/cards/burger_card1.dart
@@ -11,68 +11,75 @@ class Burger_card1 extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: ClipRRect(
-        borderRadius: BorderRadius.circular(25),
-        child: Card(
-          elevation: 5,
-          child: Container(
-            height: 256,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    height: 160,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Image.asset("assets/burger1.png"),
+          borderRadius: BorderRadius.circular(25),
+          child: Stack(
+            children: [
+              Card(
+                elevation: 5,
+                child: Container(
+                  height: 256,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  const SizedBox(height: 10),
-                  Row(
-                    children: const [
-                      Text(
-                        "Chipotle Cheesy Chicken",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 17,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 160,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Positioned(
+                              top: -10,
+                              right: 0,
+                              left: 0,
+                              child: Image.asset("assets/burger1.png")),
                         ),
-                      ),
-                      SizedBox(
-                        width: 90,
-                      ),
-                      Text(
-                        "Rs.100",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 17,
+                        const SizedBox(height: 10),
+                        Row(
+                          children: const [
+                            Text(
+                              "Chipotle Cheesy Chicken",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 90,
+                            ),
+                            Text(
+                              "Rs.100",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17,
+                              ),
+                            )
+                          ],
                         ),
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 7,
-                  ),
-                  const Text(
-                    "Chicken Burger",
-                    style: TextStyle(
-                      color: Colors.black45,
-                      fontSize: 16,
+                        const SizedBox(
+                          height: 7,
+                        ),
+                        const Text(
+                          "Chicken Burger",
+                          style: TextStyle(
+                            color: Colors.black45,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ],
+                ),
               ),
-            ),
-          ),
-        ),
-      ),
+            ],
+          )),
     );
   }
 }
--- lib/ui/burger_page.dart
+++ lib/ui/burger_page.dart
@@ -1,21 +1,89 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:foodapp/ui/home_page.dart';
 
-class BurgerPage extends StatelessWidget {
-  const BurgerPage({Key? key}) : super(key: key);
+class BurgerPage extends StatefulWidget {
+  const BurgerPage({super.key});
 
+  @override
+  State<BurgerPage> createState() => _BurgerPageState();
+}
+
+class _BurgerPageState extends State<BurgerPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: IconButton(
-            onPressed: () {
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (Context) => HomePage()));
-            },
-            icon: Icon(Icons.arrow_back_ios)),
-      ),
-    );
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          automaticallyImplyLeading: false,
+          title: Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Card(
+                  elevation: 5,
+                  child: IconButton(
+                    icon: const Icon(
+                      Icons.arrow_back_ios,
+                      size: 30,
+                      color: Colors.black,
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const HomePage(),
+                      ));
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            Card(
+              elevation: 5,
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.favorite_border_outlined,
+                  size: 35,
+                  color: Colors.red,
+                ),
+              ),
+            )
+          ],
+        ),
+        body: Center(
+          child: Column(
+            children: const [
+              SizedBox(height: 20),
+              Text(
+                "Chipotle Cheesy Chicken",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "A Signature flame-grilled chicken patty topped "
+                "with smokeed beef",
+                style: TextStyle(fontSize: 18),
+                maxLines: 2,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 120,
+              ),
+              Image(
+                image: AssetImage('assets/burger1.png'),
+                width: 250,
+              ),
+            ],
+          ),
+        ));
   }
 }
